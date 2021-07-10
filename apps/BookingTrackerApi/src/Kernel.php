33894e7874f1461c08e8e9a10a5b6162d563e509
raseal <ramon.serra@uvinum.com>
2021-07-10 16:40:54 +0200

    Change namespace for Symfony

--- apps/BookingTrackerApi/src/Kernel.php
+++ apps/BookingTrackerApi/src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SymfonyClient;
+namespace BookingTrackerApi;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
